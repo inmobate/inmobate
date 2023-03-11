82fed9f76dd6b37d3917841652f318717cc17b5f
Renzo Cervantes <r33n84@gmail.com>
2023-03-11 13:16:04 -0300

    add: init function

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function laCajaDePandora(numero) {
   // proximamente escribiremos codigo aqui
+  if (num % 2 === 0) {
+    return num.toString(2); // convierte a binario
+  } else {
+    return num.toString(16); // convierte a hexadecimal
+  }
 }
