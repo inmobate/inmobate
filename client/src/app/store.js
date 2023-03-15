a7083fb388eef0ae0ff5386b6b0f3f0699e2732d
sebacolladott <105157508+sebacolladott@users.noreply.github.com>
2023-03-15 15:16:23 -0300

    Seba

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,14 +1,22 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-/* slices(global states) */
-import { propertyToAdd } from "./slices/propertyToAdd/index.js";
+
 /* api */
 import { experimental } from "./api/experimental.js";
+
+import { propertiesId } from "./api/propertiesId.js";
+
+/* slices(global states) */
+import { propertyToAdd } from "./slices/propertyToAdd/index.js";
 
 export default configureStore({
   reducer: {
     [experimental.reducerPath]: experimental.reducer,
+    [propertiesId.reducerPath]: propertiesId.reducer,
     propertyToAdd: propertyToAdd.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(experimental.middleware),
+    getDefaultMiddleware().concat(
+      experimental.middleware,
+      propertiesId.middleware
+    ),
 });
--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -33,7 +33,7 @@ export const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/detail",
+        path: "/detail/:id",
         element: <Detail />,
       },
       {
