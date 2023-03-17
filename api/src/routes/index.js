470861826f1192698aedc40cb55dc1126d9efdf8
Jonatan-iturriago <107939873+Jonatan-iturriago@users.noreply.github.com>
2023-03-16 22:56:29 -0500

    rutas get

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,21 +1,34 @@
 const { Router } = require('express');
-const multer  = require('multer');
+// const multer  = require('multer');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-const {allUsers,postUsers,postPublications,
-    allPublications,allProperty, allSale,allBooking} = require('../handler/handlerUser.js');
-const {postComments,allType,allServicios} = require('../handler/handlerUser');
+const {allUsers,postUsers,postPublications,allPublications,allProperty, allSale,allReservas,postComments,allType,allServicios,allComments} = require('../handler/handlerUser.js');
+
 const router = Router();
 
-router.get('/property', allProperty)
-router.get('/type', allType)
-router.get('/servicio', allServicios)
-router.get('/users', allUsers)
-router.get('/publication', allPublications)
-router.get('/sale',allSale)
-router.get('/booking',allBooking)
-router.post('/users', postUsers)
-router.post('/comentarios',postComments)
-router.post('/publication', postPublications)
+router.get('/property', allProperty)//lista
+router.post('/property')
+router.put('/property')
+
+router.get('/type', allType)//lista
+router.get('/servicio', allServicios)//lista
+
+router.get('/sale',allSale)//lista
+router.post('sale')
+
+router.get('/booking',allReservas)//lista
+router.post('/booking')
+
+router.get('/users', allUsers)//lista
+router.post('/users', postUsers)//lista
+
+router.get("/comentarios",allComments)//lista
+router.post('/comentarios',postComments)//lista
+router.put("/comentarios")
+
+
+router.get('/publication', allPublications)//lista
+router.post('/publication', postPublications)// lista
+router.put('/publication')
 
 module.exports = router;
