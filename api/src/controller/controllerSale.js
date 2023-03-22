a6e519b1a3ba999a44d0206213e3771f12464a6e
sebacolladott <105157508+sebacolladott@users.noreply.github.com>
2023-03-22 01:10:27 -0300

    Seba

--- api/src/controller/controllerSale.js
+++
@@ -1,9 +0,0 @@
-const {Sale} = require('../db')
-
-
-const getVentas = async() => {
-    const Ventas = await Sale.findAll()
-    return Ventas
-}
-
-module.exports = {getVentas}
--- api/src/controller/controllerUsers.js
+++
@@ -1,9 +0,0 @@
-const {User} = require('../db')
-
-
-const getUser = async() => {
-    const usuarios = await User.findAll()
-    return usuarios
-}
-
-module.exports = {getUser}
