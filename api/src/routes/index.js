8588f158b066a161660ce1f5ee514ef108870a65
henrydjpacheco <henrydjpacheco@gmailcom>
2023-03-27 11:22:19 -0500

    jjj

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -129,7 +129,7 @@ router.get('/auth/google/callback', passport.authenticate('google', { failureRed
     }
     token = jwt.sign( payload, process.env.JWT_SECRET_KEY, { expiresIn: '1d' })
   
-    res.json({token}).redirect("/success")
+    res.redirect(`/success?${token}`)
   });
   router.get('/auth/facebook',passport.authenticate('facebook', { scope: ['email'] }));
   
