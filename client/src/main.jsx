6655a9ab39d78d498f350bbc3ae9f13f20878eaf
Renzo Cervantes <r33n84@gmail.com>
2023-03-29 18:09:40 -0300

    vuelta atras

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,13 +11,11 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const persistor = persistStore(store);
 
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Auth0Provider
-      domain={process.env.VITE_AUTH0_DOMAIN}
-      clientId={process.env.VITE_AUTH0_CLIENT_ID}
+      domain="dev-jkdom0hu2zoe2dm8.us.auth0.com"
+      clientId="jdILASD5sHvuzEUIS0pI1i0kyrRToZDV"
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
